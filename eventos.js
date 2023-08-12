01384111890fa1b438c80eb6cf60ce350aa30672
antovidal13 <avtichu21@gmail.com>
2023-08-11 21:19:36 -0300

    solucion al click

--- eventos.js
+++ eventos.js
@@ -5,3 +5,18 @@ div.forEach(function(div){
         alert('Hola! Soy el div');
     });
 });
+
+
+//solucion-click
+const container=document.getElementById('container');
+const boton=document.getElementById('boton');
+
+container.addEventListener('click',function(event){
+    event.stopPropagation();
+});
+
+boton.addEventListener('click', function(event){
+    event.stopPropagation();
+});
+
+
